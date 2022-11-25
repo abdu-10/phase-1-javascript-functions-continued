17ec42c8b38885a8b863ca3e37ba00e46624fa9b
abdulrahman <abdulrahman.omar@student.moringschool.com>
2022-11-25 12:16:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(thing="roller-skate"){
+    return `This Saturday, I want to ${thing}!`
+}
+
+function mondayWork(thing="go to the office"){
+    return `This Monday, I will ${thing}.`}
+
+
+    function wrapAdjective(smart="*"){
+        return function(block="special"){
+            return `You are ${smart}${block}${smart}!`
+        }
+
+        }
+
+        
+    
